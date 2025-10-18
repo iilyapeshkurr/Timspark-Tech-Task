8c24c01b1de67ed3837f767b9f2486bd7307440d
ilyapeshkur-inno <ilya.peshkur@innowise.com>
2025-10-18 17:03:13 +0300

    Adding serilog

--- IpLookupService/Program.cs
+++ IpLookupService/Program.cs
@@ -1,5 +1,20 @@
+using DotNetEnv;
+
 var builder = WebApplication.CreateBuilder(args);
 
+Env.Load();
+
+builder.Configuration.AddEnvironmentVariables();
+
+builder.Host.ConfigureSerilog();
+builder.Services.ConfigureIpStack(builder.Configuration);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseExceptionHandler();
 
 app.Run();
